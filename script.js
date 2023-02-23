afcd19e7358e94c6ce85b0d8d3e3717d7276160e
Nicla <niclagallicchio@gmail.com>
2023-02-23 19:30:08 +0100

    navbar scroll finita + aggiunto mediaquery

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Sto lavorando");
+document.addEventListener("scroll", changeNavbar);
+
+function changeNavbar(event) {
+    const mainNavbar = document.querySelector("#mainNavbar")
+    if (window.scrollY > 100) {
+        mainNavbar.classList.add("thin");
+        mainNavbar.classList.add("bg-white");
+    } else {
+        mainNavbar.classList.remove("thin");
+        mainNavbar.classList.remove("bg-white")
+    }
+}
